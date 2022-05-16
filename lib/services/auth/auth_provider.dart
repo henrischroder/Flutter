4ba894f348eb4df44df0b385f0fa18c26b964820
Henri <henri@henri.henri>
2022-05-16 20:44:46 +0200

    step-8

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,13 @@
+import 'package:tutorial/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> login({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+}
