c0c0fd71130966217183b5e283fc413e6c78cab3
Henri <henri@henri.henri>
2022-05-23 14:52:06 +0200

    step-13

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -316,18 +316,18 @@ const textColumn = 'text';
 const isSyncedWithCloudColumn = 'is_synced_with_cloud';
 const createUserTable = '''
 CREATE TABLE IF NOT EXISTS "user" (
- "id" INTEGER NOT NULL,
- "email" TEXT NOT NULL UNIQUE,
- PRIMARY KEY("id" AUTOINCREMENT),
+	"id"	INTEGER NOT NULL,
+	"email"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 ''';
 const createNoteTable = '''
 CREATE TABLE IF NOT EXISTS "note" (
-  "id" INTEGER NOT NULL,
-  "user_id" INTEGER NOT NULL,
-  "text" TEXT,
-  "is_synced_with_cloud" INTEGER NOT NULL DEFAULT 0,
-  PRIMARY KEY("id" AUTOINCREMENT),
-  FOREIGN KEY("user_id") REFERENCES "user"("id")
+	"id"	INTEGER NOT NULL,
+	"user_id"	INTEGER NOT NULL,
+	"text"	TEXT,
+	"is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("user_id") REFERENCES "user"("id")
 );
 ''';
